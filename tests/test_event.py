a141e9e553a837db6879e9842600746e1d160d5b
백세종 <sejong418@icloud.com>
2024-09-06 01:57:07 +0900

    test: 테스트코드 fixture 주입 수정

--- tests/test_event.py
+++ tests/test_event.py
@@ -7,11 +7,15 @@ from spakky.domain.ports.event.event_publisher import (
     IAsyncEventPublisher,
     IEventPublisher,
 )
+from spakky.threading.managed_thread import AsyncManagedThread, ManagedThread
 
 from tests.apps.dummy import DummyEventHandler, SampleEvent
 
 
-def test_synchronous_event_publish_and_consume(context: ApplicationContext) -> None:
+def test_synchronous_event_publish_and_consume(
+    context: ApplicationContext, managed_thread: ManagedThread
+) -> None:
+    managed_thread.start()
     publisher = context.get(IEventPublisher)
     publisher.publish(SampleEvent(message="Hello, World!"))
     publisher.publish(SampleEvent(message="Goodbye, World!"))
@@ -22,8 +26,9 @@ def test_synchronous_event_publish_and_consume(context: ApplicationContext) -> N
 
 @pytest.mark.asyncio
 async def test_asynchronous_event_publish_and_consume(
-    context: ApplicationContext,
+    context: ApplicationContext, async_managed_thread: AsyncManagedThread
 ) -> None:
+    async_managed_thread.start()
     publisher = context.get(IAsyncEventPublisher)
     await publisher.publish(SampleEvent(message="Hello, World!"))
     await publisher.publish(SampleEvent(message="Goodbye, World!"))
