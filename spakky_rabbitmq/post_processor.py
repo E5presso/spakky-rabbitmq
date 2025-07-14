2198d1495187af5dba53de55e24463621efd241d
Spakky <sejong418@icloud.com>
2025-07-14 19:12:48 +0900

    feat: BackgroundService 추상클래스를 상속받아, 이벤트 컨슈머 구현 변경

--- spakky_rabbitmq/post_processor.py
+++ spakky_rabbitmq/post_processor.py
@@ -1,37 +1,76 @@
-from inspect import ismethod, getmembers, iscoroutinefunction
+from functools import wraps
+from inspect import getmembers, iscoroutinefunction, ismethod
 from logging import Logger
+from typing import Any
 
-from spakky.application.interfaces.container import IPodContainer
-from spakky.application.interfaces.post_processor import IPodPostProcessor
-from spakky.domain.models.event import DomainEvent
+from spakky.domain.models.event import AbstractDomainEvent
 from spakky.domain.ports.event.event_consumer import IAsyncEventConsumer, IEventConsumer
-from spakky.pod.order import Order
+from spakky.pod.annotations.order import Order
+from spakky.pod.annotations.pod import Pod
+from spakky.pod.interfaces.aware.container_aware import IContainerAware
+from spakky.pod.interfaces.aware.logger_aware import ILoggerAware
+from spakky.pod.interfaces.container import IContainer
+from spakky.pod.interfaces.post_processor import IPostProcessor
 from spakky.stereotype.event_handler import EventHandler, EventRoute
 
 
 @Order(1)
-class RabbitMQPostProcessor(IPodPostProcessor):
+@Pod()
+class RabbitMQPostProcessor(IPostProcessor, ILoggerAware, IContainerAware):
     __logger: Logger
+    __container: IContainer
 
-    def __init__(self, logger: Logger) -> None:
-        super().__init__()
+    def set_logger(self, logger: Logger) -> None:
         self.__logger = logger
 
-    def post_process(self, container: IPodContainer, pod: object) -> object:
+    def set_container(self, container: IContainer) -> None:
+        self.__container = container
+
+    def post_process(self, pod: object) -> object:
         if not EventHandler.exists(pod):
             return pod
-        consumer = container.get(IEventConsumer)
-        async_consumer = container.get(IAsyncEventConsumer)
-        for _, method in getmembers(pod, ismethod):
-            route: EventRoute[DomainEvent] | None = EventRoute.get_or_none(method)
+        handler: EventHandler = EventHandler.get(pod)
+        consumer = self.__container.get(IEventConsumer)
+        async_consumer = self.__container.get(IAsyncEventConsumer)
+        for name, method in getmembers(pod, ismethod):
+            route: EventRoute[AbstractDomainEvent] | None = EventRoute[
+                AbstractDomainEvent
+            ].get_or_none(method)
             if route is None:
                 continue
             # pylint: disable=line-too-long
             self.__logger.info(
                 f"[{type(self).__name__}] {route.event_type.__name__} -> {method.__qualname__}"
             )
+
             if iscoroutinefunction(method):
-                async_consumer.register(route.event_type, method)
+
+                @wraps(method)
+                async def async_endpoint(
+                    *args: Any,
+                    method_name: str = name,
+                    controller_type: type[object] = handler.type_,
+                    context: IContainer = self.__container,
+                    **kwargs: Any,
+                ) -> Any:
+                    controller_instance = context.get(controller_type)
+                    method_to_call = getattr(controller_instance, method_name)
+                    return await method_to_call(*args, **kwargs)
+
+                async_consumer.register(route.event_type, async_endpoint)
                 continue
-            consumer.register(route.event_type, method)
+
+            @wraps(method)
+            def endpoint(
+                *args: Any,
+                method_name: str = name,
+                controller_type: type[object] = handler.type_,
+                context: IContainer = self.__container,
+                **kwargs: Any,
+            ) -> Any:
+                controller_instance = context.get(controller_type)
+                method_to_call = getattr(controller_instance, method_name)
+                return method_to_call(*args, **kwargs)
+
+            consumer.register(route.event_type, endpoint)
         return pod
--- tests/test_event.py
+++ tests/test_event.py
@@ -1,8 +1,8 @@
+from asyncio import sleep as asleep  # type: ignore
 from time import sleep
-from asyncio import sleep as asleep
 
 import pytest
-from spakky.application.application_context import ApplicationContext
+from spakky.application.application import SpakkyApplication
 from spakky.domain.ports.event.event_publisher import (
     IAsyncEventPublisher,
     IEventPublisher,
@@ -11,20 +11,20 @@ from spakky.domain.ports.event.event_publisher import (
 from tests.apps.dummy import DummyEventHandler, SampleEvent
 
 
-def test_synchronous_event(context: ApplicationContext) -> None:
-    publisher = context.get(IEventPublisher)
+def test_synchronous_event(app: SpakkyApplication) -> None:
+    publisher = app.container.get(IEventPublisher)
     publisher.publish(SampleEvent(message="Hello, World!"))
     publisher.publish(SampleEvent(message="Goodbye, World!"))
     sleep(0.1)
-    handler = context.get(DummyEventHandler)
+    handler = app.container.get(DummyEventHandler)
     assert handler.count == 2
 
 
 @pytest.mark.asyncio
-async def test_asynchronous_event(context: ApplicationContext) -> None:
-    publisher = context.get(IAsyncEventPublisher)
+async def test_asynchronous_event(app: SpakkyApplication) -> None:
+    publisher = app.container.get(IAsyncEventPublisher)
     await publisher.publish(SampleEvent(message="Hello, World!"))
     await publisher.publish(SampleEvent(message="Goodbye, World!"))
     await asleep(0.1)
-    handler = context.get(DummyEventHandler)
+    handler = app.container.get(DummyEventHandler)
     assert handler.count == 2
